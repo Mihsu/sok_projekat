dc370ff4f7303130039a7ac8a61ed06cf99f7097
Mihsu <mihabubnjevic@gmail.com>
2021-02-14 02:54:55 +0100

    Finished version, MAE: 3.5

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import imutils
 import numpy
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
 
 
 def count_people(video_path):
@@ -18,8 +19,8 @@ def count_people(video_path):
 
     while video.isOpened():
         flag, video_frame = video.read()
-        # if not flag:
-        #   break
+        if not flag:
+            break
         resized_frame = define_region_of_interest(video_frame, x_start_plateau, y_start_plateau, plateau_width,
                                                   plateau_height)
 
@@ -33,13 +34,13 @@ def count_people(video_path):
 
         distance = cv2.absdiff(init_frame, gb_frame)
 
-        _, threshold_frame = cv2.threshold(distance, 21, 255, cv2.THRESH_BINARY)
+        _, threshold_frame = cv2.threshold(distance, 13, 255, cv2.THRESH_BINARY)
         dilated_frame = cv2.dilate(threshold_frame, None, iterations=2)
 
-        _, contours, _ = cv2.findContours(dilated_frame, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        _, contours, _ = cv2.findContours(dilated_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
-            if cv2.contourArea(contour) < 150:
+            if cv2.contourArea(contour) < 120:
                 continue
             x, y, width, height = cv2.boundingRect(contour)
 
@@ -53,17 +54,19 @@ def count_people(video_path):
             center_point = (center_point_x_int, center_point_y_int)
             cv2.circle(resized_frame, center_point, 1, (0, 0, 255), 2)
 
-            cv2.line(resized_frame, (0, 250), (500, 250), (0, 0, 255), 2)
+            cv2.line(resized_frame, (0, plateau_height // 3 ), (plateau_width, plateau_height // 3), (0, 0, 255), 2)
+            if crossed_threshold(center_point_y, plateau_height / 3):
+                counted_people += 1
 
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        cv2.imshow("Peoples", resized_frame)
-        cv2.waitKey(0)
+            # plt.imshow(cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB))
+            # plt.show()
     print(counted_people)
-    video.release()
-    cv2.destroyAllWindows()
-
     return counted_people
+
+
+def return_mae(given, final):
+    mae = mean_absolute_error(given, final)
+    return mae
 
 
 def do_gaussian_blur_frame(frame):
@@ -80,14 +83,25 @@ def define_region_of_interest(frame, x, y, width, height):
 
 
 # koordinatni sistem ide od gore levo
-def crossed_threshold(x, y):
-    threshold = 255
-    res = y - threshold
+def crossed_threshold(y, y_center_frame):
+    res = y - y_center_frame
     if abs(res) <= 1:
         return True
     return False
 
 
+def load_results():
+    results = [count_people("Videos/video1.mp4"), count_people("Videos/video2.mp4"),
+               count_people("Videos/video3.mp4"), count_people("Videos/video4.mp4"),
+               count_people("Videos/video5.mp4"), count_people("Videos/video6.mp4"),
+               count_people("Videos/video7.mp4"), count_people("Videos/video8.mp4"),
+               count_people("Videos/video9.mp4"), count_people("Videos/video10.mp4")]
+    return results
+
+
 if __name__ == '__main__':
-    count_people("Videos/video2.mp4")
-    print('nothing for now')
+    given_results = [4, 24, 17, 23, 17, 27, 29, 22, 10, 23]
+    final_results = load_results()
+
+    print("Mae:")
+    print(str(return_mae(given_results, final_results)))
